c3f24951f06b82af8a28a513d907e58ffd0855e0
levrozhkov-dev <levrozhkov89@gmail.com>
2026-06-20 02:17:32 +0300

    Fix: auth

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -2,26 +2,30 @@ import { Router } from 'express'
 import {
     getCurrentUser,
     getCurrentUserRoles,
+    getCsrfToken,
     login,
     logout,
     refreshAccessToken,
     register,
     updateCurrentUser,
-    getCsrfToken
 } from '../controllers/auth'
 import auth from '../middlewares/auth'
 import csrfProtection from '../middlewares/csrf'
-import { validateUserUpdateBody } from '../middlewares/validations'
+import {
+    validateAuthentication,
+    validateUserBody,
+    validateUserUpdateBody,
+} from '../middlewares/validations'
 
 const authRouter = Router()
 
 authRouter.get('/user', auth, getCurrentUser)
 authRouter.patch('/me', auth, validateUserUpdateBody, updateCurrentUser)
 authRouter.get('/user/roles', auth, getCurrentUserRoles)
-authRouter.post('/login', login)
+authRouter.get('/csrf-token', getCsrfToken)
+authRouter.post('/login', validateAuthentication, login)
 authRouter.post('/token', csrfProtection, refreshAccessToken)
 authRouter.post('/logout', csrfProtection, logout)
-authRouter.post('/register', register)
-authRouter.get('/csrf-token', getCsrfToken)
+authRouter.post('/register', validateUserBody, register)
 
 export default authRouter
